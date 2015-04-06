e8e3671717dbd32e11efcee2fb7621c6c5395a75
Yskinator <yski2moreletters@gmail.com>
2015-04-06 14:49:09 +0300

    Turned Entity into a class instead of abstract class. Added RigidBody and related functions to Entity.

--- Keplerian/src/main/java/keplerian/gameFramework/Entity.java
+++ Keplerian/src/main/java/keplerian/gameFramework/Entity.java
@@ -6,10 +6,53 @@
 
 package keplerian.gameFramework;
 
+import keplerian.physics.RigidBody;
+
 /**
  * Anything the user might be interested in.
  * @author Ville-Matti Tanninen
  */
-public abstract class Entity {
+public class Entity {
+    /**
+     * A constant used for entities without rigid bodies.
+     */
+    public final static RigidBody NO_RIGIDBODY = null;
     
+    /**
+     * The rigid body of the entity, if it has one.
+     */
+    private RigidBody rigidBody;
+
+    /**
+     * Constructs a new entity with a rigid body.
+     * @param rigidBody The rigidBody of the entity.
+     */
+    public Entity(RigidBody rigidBody) {
+        this.rigidBody = rigidBody;
+    }
+    
+    
+    
+    /**
+     * Used to check if the entity has a rigid body.
+     * @return Whether the entity has a rigid body or not.
+     */
+    public boolean hasRigidBody()
+    {
+        if(this.getRigidBody() != NO_RIGIDBODY)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * @return the rigidBody
+     */
+    public RigidBody getRigidBody() {
+        return rigidBody;
+    }
 }
