0e94fda7c4bf00d8b3c835b1f4c733b97de00996
Yskinator <yski2moreletters@gmail.com>
2015-04-27 22:48:09 +0300

    Added GUIScene, a Scene that uses GUI as its user interface.

---
+++ Keplerian/src/main/java/keplerian/gameFramework/GUIScene.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package keplerian.gameFramework;
+
+/**
+ *
+ * @author Ville-Matti
+ */
+public class GUIScene extends Scene {
+    
+    /**
+     * Constructs a new GameScene.
+     */
+    public GUIScene()
+    {
+        this.UI = new GUI();
+    }
+    
+    protected void initEntities()
+    {
+        GUI gui = (GUI) UI;
+        gui.init(entities);
+    }
+    
+    @Override
+    public void update() {
+        GUI gui = (GUI) UI;
+        gui.updateSpritePositions(entities);
+        
+    }
+
+    @Override
+    public void processInput() {
+        GUI gui = (GUI) UI;
+        gui.getInput();
+        if(gui.windowClosed())
+        {
+            exit();
+        }
+    }
+
+    @Override
+    public void show() {
+        GUI gui = (GUI) UI;
+        gui.show(entities);
+    }
+
+    @Override
+    public void exit() {
+        GUI gui = (GUI) UI;
+        gui.close();
+        currScene = NO_SCENE;
+    }
+    
+}
